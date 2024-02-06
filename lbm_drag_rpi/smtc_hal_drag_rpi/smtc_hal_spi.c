a7710d5b5567d1830513e2e6973bde60301e2178
Alessandro Aimi <alleaimi95@gmail.com>
2024-02-06 08:53:56 +0100

    SPI

---
+++ lbm_drag_rpi/smtc_hal_drag_rpi/smtc_hal_spi.c
@@ -0,0 +1,101 @@
+/*!
+ * \file      smtc_hal_spi.c
+ *
+ * \brief     SPI Hardware Abstraction Layer implementation
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Alessandro Aimi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- DEPENDENCIES ------------------------------------------------------------
+ */
+
+#include <stdbool.h> // bool type
+
+#include "smtc_hal_spi.h"
+#include "smtc_hal_mcu.h"
+#include <pigpio.h>
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE MACROS-----------------------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE CONSTANTS -------------------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE TYPES -----------------------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE VARIABLES -------------------------------------------------------
+ */
+
+static int spi_handle;
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE FUNCTIONS DECLARATION -------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PUBLIC FUNCTIONS DEFINITION ---------------------------------------------
+ */
+
+void hal_spi_init(const uint32_t id, const hal_gpio_pin_names_t mosi, const hal_gpio_pin_names_t miso,
+                  const hal_gpio_pin_names_t sclk)
+{
+    spi_handle = spiOpen(0, 500000, 0);
+    if (spi_handle < 0)
+    {
+        mcu_panic();
+    }
+}
+
+void hal_spi_de_init(const uint32_t id)
+{
+    if (spiClose(spi_handle) != 0)
+    {
+        mcu_panic();
+    }
+}
+
+uint16_t hal_spi_in_out(const uint32_t id, const uint16_t out_data)
+{
+    char in_buf;
+    char out_buf = (char)(out_data & 0xFF);
+    if (spiXfer(spi_handle, &out_buf, &in_buf, 1) != 1)
+    {
+        mcu_panic();
+    }
+    return in_buf;
+}
+
+/* --- EOF ------------------------------------------------------------------ */
