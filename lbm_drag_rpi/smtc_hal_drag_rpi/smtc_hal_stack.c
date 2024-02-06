79d9cc46dfb5c2b215c32e0ea882bb26755d341c
Alessandro Aimi <alleaimi95@gmail.com>
2024-02-06 14:58:45 +0100

    context store/restore in stack

---
+++ lbm_drag_rpi/smtc_hal_drag_rpi/smtc_hal_stack.c
@@ -0,0 +1,94 @@
+/*!
+ * \file      smtc_hal_stack.c
+ *
+ * \brief     STACK Hardware Abstraction Layer implementation
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Alessandro Aimi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- DEPENDENCIES ------------------------------------------------------------
+ */
+
+#include <stdint.h>  // C99 types
+#include <stdbool.h> // bool type
+
+#include "smtc_hal_stack.h"
+
+#include <string.h> // memcpy
+#include <assert.h> // assert
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE MACROS-----------------------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE CONSTANTS -------------------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE TYPES -----------------------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE VARIABLES -------------------------------------------------------
+ */
+
+static uint8_t memory[MEM_SIZE];
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE FUNCTIONS DECLARATION -------------------------------------------
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PUBLIC FUNCTIONS DEFINITION ---------------------------------------------
+ */
+
+void hal_stack_write_buffer(uint32_t addr, const uint8_t *buffer, uint32_t size)
+{
+    // segfault protection, could still overwrite
+    assert(size < MEM_SIZE && addr < MEM_SIZE - size);
+    assert(sizeof(buffer) >= size);
+    memcpy(memory + addr, buffer, size);
+}
+
+void hal_stack_read_buffer(uint32_t addr, uint8_t *buffer, uint32_t size)
+{
+    assert(size < MEM_SIZE && addr < MEM_SIZE - size);
+    assert(sizeof(buffer) >= size);
+    memcpy(buffer, memory + addr, size);
+}
+
+/*
+ * -----------------------------------------------------------------------------
+ * --- PRIVATE FUNCTIONS DEFINITION --------------------------------------------
+ */
+
+/* --- EOF ------------------------------------------------------------------ */
